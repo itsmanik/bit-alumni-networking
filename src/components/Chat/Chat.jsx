7fee67158f1704370aa77ff7148aa64a13d11487
itsmanik <manik.hilalpure@gmail.com>
2025-01-15 11:51:30 +0530

    added final touches'

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -5,42 +5,42 @@ import ChatWindow from './ChatWindow';
 const defaultChats = [
   {
     id: 1,
-    name: 'Elon Musk',
+    name: 'Satish M H',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfoHR4N4fK95SQ6cyL0knP9vAMcWa2hrjNh2lv-c4wbA&s',
     unread: 0,
     messages: [
-      { me: true, text: 'Hey Elon!', time: '11:45 AM' },
-      { me: false, text: 'Hello!', time: '11:50 AM' },
+      { me: true, text: 'Hey Elon! We should organize an alumni mentorship program.', time: '11:45 AM' },
+      { me: false, text: 'That sounds like a great idea! I’m in.', time: '11:50 AM' },
     ],
   },
   {
     id: 2,
-    name: 'Jeff Bezos',
+    name: 'Richard Bezos',
     image: 'https://th.bing.com/th?q=Profile+for+Boys&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247',
     unread: 1,
     messages: [
-      { me: true, text: 'Hey Jeff, what’s up?', time: '12:00 PM' },
-      { me: false, text: 'Not much, just relaxing. How about you?', time: '12:05 PM' },
+      { me: true, text: 'Hey Jeff, are you attending the alumni networking event next week?', time: '12:00 PM' },
+      { me: false, text: 'I’ll be there! Looking forward to catching up with everyone.', time: '12:05 PM' },
     ],
   },
   {
     id: 3,
-    name: 'Bill Gates',
+    name: 'Bill James',
     image: 'https://th.bing.com/th/id/OIP.YDyoIafIwW1tILED3HgZRQHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7',
     unread: 2,
     messages: [
-      { me: true, text: 'Hey Bill, did you see the new tech announcement?', time: '1:00 PM' },
-      { me: false, text: 'Yes, it’s exciting! I’ve been reading up on it.', time: '1:02 PM' },
-      { me: true, text: 'Let’s discuss it tomorrow?', time: '1:05 PM' },
+      { me: true, text: 'Hey Bill, I was thinking we could host a webinar for alumni career advice.', time: '1:00 PM' },
+      { me: false, text: 'Sounds like a fantastic idea! Let’s get started on that.', time: '1:02 PM' },
+      { me: true, text: 'Let’s discuss the details tomorrow.', time: '1:05 PM' },
     ],
   },
   {
     id: 4,
-    name: 'Sundar Pichai',
+    name: 'Ali Azad',
     image: 'https://th.bing.com/th/id/OIP.WazsBZZNILHEXLo0CyWlpAHaJO?w=136&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
     unread: 0,
     messages: [
-      { me: true, text: 'Sundar, are we on for the meeting at 3?', time: '2:30 PM' },
+      { me: true, text: 'Sundar, are we on for the alumni event planning meeting at 3?', time: '2:30 PM' },
       { me: false, text: 'Yes, looking forward to it!', time: '2:32 PM' },
     ],
   },
@@ -50,10 +50,10 @@ const defaultChats = [
     image: 'https://th.bing.com/th?q=Profile+for+Boys&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247',
     unread: 3,
     messages: [
-      { me: true, text: 'Hey Mark, have you seen the new updates for Facebook?', time: '3:00 PM' },
-      { me: false, text: 'Yes, some interesting changes. Let’s chat more about it later.', time: '3:05 PM' },
-      { me: true, text: 'Cool, I’ll catch up with you tomorrow then.', time: '3:10 PM' },
-      { me: false, text: 'Sounds good!', time: '3:15 PM' },
+      { me: true, text: 'Hey Mark, I wanted to ask if you’d be interested in sponsoring an alumni event?', time: '3:00 PM' },
+      { me: false, text: 'I’d be happy to help out with that. Let’s chat more about it soon.', time: '3:05 PM' },
+      { me: true, text: 'Great! I’ll send over the details tomorrow.', time: '3:10 PM' },
+      { me: false, text: 'Looking forward to it!', time: '3:15 PM' },
     ],
   },
   {
@@ -62,8 +62,8 @@ const defaultChats = [
     image: 'https://th.bing.com/th/id/OIP.WazsBZZNILHEXLo0CyWlpAHaJO?w=136&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
     unread: 0,
     messages: [
-      { me: true, text: 'Tim, when is the next Apple event?', time: '4:00 PM' },
-      { me: false, text: 'It’s scheduled for next week. I’ll send you the invite.', time: '4:10 PM' },
+      { me: true, text: 'Tim, when is the next alumni event for tech leaders?', time: '4:00 PM' },
+      { me: false, text: 'It’s scheduled for next week. I’ll send you the invite soon.', time: '4:10 PM' },
     ],
   },
   {
@@ -72,10 +72,10 @@ const defaultChats = [
     image: 'https://th.bing.com/th/id/OIP.YDyoIafIwW1tILED3HgZRQHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7',
     unread: 4,
     messages: [
-      { me: true, text: 'Satya, do you have any updates on Azure?', time: '5:00 PM' },
-      { me: false, text: 'I’m currently working on some enhancements, will update soon.', time: '5:05 PM' },
-      { me: true, text: 'Great, let’s catch up in the evening.', time: '5:10 PM' },
-      { me: false, text: 'Sure, talk to you later!', time: '5:15 PM' },
+      { me: true, text: 'Satya, would you be interested in mentoring recent graduates from our alumni network?', time: '5:00 PM' },
+      { me: false, text: 'I’d love to! Let’s discuss how I can help.', time: '5:05 PM' },
+      { me: true, text: 'Fantastic, I’ll get in touch with the team tomorrow.', time: '5:10 PM' },
+      { me: false, text: 'Looking forward to it!', time: '5:15 PM' },
     ],
   },
   {
@@ -84,8 +84,8 @@ const defaultChats = [
     image: 'https://th.bing.com/th/id/OIP.WazsBZZNILHEXLo0CyWlpAHaJO?w=136&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
     unread: 0,
     messages: [
-      { me: true, text: 'Sheryl, are we ready for the board meeting?', time: '6:00 PM' },
-      { me: false, text: 'Yes, everything is set. I’ll see you there.', time: '6:05 PM' },
+      { me: true, text: 'Sheryl, are we ready for the alumni fundraising campaign launch?', time: '6:00 PM' },
+      { me: false, text: 'Yes, everything is set. I’ll see you at the launch event.', time: '6:05 PM' },
     ],
   },
   {
@@ -94,8 +94,8 @@ const defaultChats = [
     image: 'https://th.bing.com/th?q=Profile+for+Boys&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247',
     unread: 1,
     messages: [
-      { me: true, text: 'Hey Larry, did you check out the new Google project?', time: '7:00 PM' },
-      { me: false, text: 'Yes, it looks amazing. We’ll discuss it tomorrow!', time: '7:05 PM' },
+      { me: true, text: 'Hey Larry, how do you think we can improve our alumni events for future years?', time: '7:00 PM' },
+      { me: false, text: 'We can make them more interactive and include more career resources.', time: '7:05 PM' },
     ],
   },
   {
@@ -104,15 +104,15 @@ const defaultChats = [
     image: 'https://th.bing.com/th/id/OIP.YDyoIafIwW1tILED3HgZRQHaHa?w=193&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7',
     unread: 2,
     messages: [
-      { me: true, text: 'Jack, have you seen the latest Twitter update?', time: '8:00 PM' },
-      { me: false, text: 'Yes, I’m going through it now. Let’s talk about it later.', time: '8:05 PM' },
-      { me: true, text: 'Sure, let’s catch up tomorrow!', time: '8:10 PM' },
+      { me: true, text: 'Jack, let’s discuss how to get more alumni involved in our mentorship program.', time: '8:00 PM' },
+      { me: false, text: 'I’m on board. Let’s brainstorm some ideas for recruitment.', time: '8:05 PM' },
+      { me: true, text: 'Great! I’ll send some initial thoughts tomorrow.', time: '8:10 PM' },
     ],
   },
 ];
 
 const Chat = () => {
-  const [selectedChat, setSelectedChat] = useState(5);
+  const [selectedChat, setSelectedChat] = useState(7);
 
   return (
     <div className="chat flex flex-row w-full h-full">
@@ -123,6 +123,5 @@ const Chat = () => {
     </div>
   );
 };
-
 
 export default Chat;
